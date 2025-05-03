0146f446acf586a4a093ce7add707c8face57f0e
Irina Gavriukova <irina.gavriukova1991@gmail.com>
2025-05-04 02:48:46 +0300

    first-commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,51 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+// Хендлер для эндпоинта /upload
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to get file from form", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Unable to read file", http.StatusBadRequest)
+		return
+	}
+
+	result, err := service.Convert(string(data))
+	if err != nil {
+		http.Error(w, "Conversion error", http.StatusBadRequest)
+		return
+	}
+
+	filename := fmt.Sprintf("%s.txt", time.Now().UTC().Format("2006-01-02_15-04-05"))
+	err = os.WriteFile(filename, []byte(result), 0644)
+	if err != nil {
+		http.Error(w, "Unable to write result to file", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Conversion successful! Result saved to %s", filename)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers" // Импортируем пакет handlers
+)
+
+// Структура сервера с логгером и HTTP-сервером
+type Server struct {
+	logger *log.Logger
+	srv    *http.Server
+}
+
+// Функция для создания нового сервера с роутером и хендлерами
+func NewServer(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.IndexHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{logger: logger, srv: srv}
+}
+
+// Метод для запуска сервера
+func (s *Server) Start() error {
+	return s.srv.ListenAndServe()
+}
