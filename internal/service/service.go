2e961305744b79f371f94332bf6f5c91386991a1
Irina Gavriukova <irina.gavriukova1991@gmail.com>
2025-05-04 05:25:16 +0300

    3-commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -7,25 +7,32 @@ import (
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 )
 
-func Convert(data string) (string, error) {
-	if len(data) == 0 {
-		return "", errors.New("no data to convert")
+var ErrInvalidInput = errors.New("invalid input: not Morse code or plain text")
+
+func Convert(input string) (string, error) {
+
+	input = strings.TrimSpace(input)
+
+	// Проверяем, является ли строка кодом Морзе
+	if isMorseCode(input) {
+		return morse.ToText(input), nil
 	}
 
-	var result string
+	// Если это не код Морзе, пробуем конвертировать его в код Морзе
+	return morse.ToMorse(input), nil
+}
 
-	if strings.ContainsAny(data, ".- ") {
-		// Если это код Морзе, пытаемся конвертировать в текст
-		result = morse.ToText(data)
-		if result == "" { // Проверяем на пустую строку как индикатор ошибки
-			return "", errors.New("the response does not contain the expected Morse code")
-		}
-	} else {
-		// Если это текст, пытаемся конвертировать в код Морзе
-		result = morse.ToMorse(data)
-		if result == "" { // Проверяем на пустую строку как индикатор ошибки
-			return "", errors.New("failed to convert text to morse code")
+func isMorseCode(s string) bool {
+	// Разделяем строку на слова по пробелам
+	words := strings.Fields(s)
+
+	for _, word := range words {
+		// Проверяем каждый символ в слове
+		for _, char := range word {
+			if char != '.' && char != '-' && char != ' ' {
+				return false // Если есть недопустимый символ, возвращаем false
+			}
 		}
 	}
-	return result, nil
+	return true // Все символы допустимы для кода Морзе
 }
