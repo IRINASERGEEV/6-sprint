9ac77306deac7a779e63000a076357ab4e218a6b
Irina Gavriukova <irina.gavriukova1991@gmail.com>
2025-05-04 04:57:49 +0300

    second-commit

--- internal/service/service.go
+++ internal/service/service.go
@@ -15,9 +15,17 @@ func Convert(data string) (string, error) {
 	var result string
 
 	if strings.ContainsAny(data, ".- ") {
+		// Если это код Морзе, пытаемся конвертировать в текст
 		result = morse.ToText(data)
+		if result == "" { // Проверяем на пустую строку как индикатор ошибки
+			return "", errors.New("the response does not contain the expected Morse code")
+		}
 	} else {
+		// Если это текст, пытаемся конвертировать в код Морзе
 		result = morse.ToMorse(data)
+		if result == "" { // Проверяем на пустую строку как индикатор ошибки
+			return "", errors.New("failed to convert text to morse code")
+		}
 	}
 	return result, nil
 }
