9cbc9e677bcf029de228f7f2fe88c5fe5acc211f
Irina Gavriukova <irina.gavriukova1991@gmail.com>
2025-05-13 00:09:04 +0300

    11-commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,11 +10,12 @@ import (
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
 )
 
+// Парсим html-форму из файла index.html
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-// Хендлер для эндпоинта /upload
+// Получаем файл из формы и закрываем
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusBadRequest)
@@ -23,30 +24,30 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	file, _, err := r.FormFile("file")
 	if err != nil {
-		http.Error(w, "Error retrieving the file", http.StatusOK)
-		fmt.Println("Error retrieving the file:", err)
+		fmt.Println(w)
 		return
 	}
 	defer file.Close()
 
+	//Читаем данные из файла
 	data, err := io.ReadAll(file)
 	if err != nil {
 		http.Error(w, "Unable to read file", http.StatusBadRequest)
 		return
 	}
 
+	//Передаем данные в функцию конвертации и получаем результат конвертации
 	result, err := service.Convert(string(data))
 	if err != nil {
-		http.Error(w, "Conversion error", http.StatusBadRequest)
-		fmt.Println("Conversion error:", err)
 		fmt.Println(string(data))
 		return
 	}
 
+	//Записываем в локальный файл результат конвертации строки
 	filename := fmt.Sprintf("%s.txt", time.Now().UTC().Format("2006-01-02_15-04-05"))
 	err = os.WriteFile(filename, []byte(result), 0644)
 	if err != nil {
-		fmt.Println(data)
+		fmt.Println(string(data))
 		return
 	}
 	fmt.Println(result)
